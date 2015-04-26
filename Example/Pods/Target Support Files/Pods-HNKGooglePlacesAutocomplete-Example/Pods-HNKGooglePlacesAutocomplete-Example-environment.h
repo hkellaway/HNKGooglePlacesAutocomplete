91f99f8a630600a2dbcf84c2168b385d5421e788
harlan <hkellaway.tech@gmail.com>
2015-04-26 12:31:11 -0700

    Added Kiwi to Example podfile and updated Pods

--- Example/Pods/Target Support Files/Pods-HNKGooglePlacesAutocomplete-Example/Pods-HNKGooglePlacesAutocomplete-Example-environment.h
+++ Example/Pods/Target Support Files/Pods-HNKGooglePlacesAutocomplete-Example/Pods-HNKGooglePlacesAutocomplete-Example-environment.h
@@ -12,3 +12,15 @@
 #define COCOAPODS_VERSION_MINOR_HNKGooglePlacesAutocomplete 1
 #define COCOAPODS_VERSION_PATCH_HNKGooglePlacesAutocomplete 0
 
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 5
+#define COCOAPODS_VERSION_PATCH_Mantle 4
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 5
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 4
+
