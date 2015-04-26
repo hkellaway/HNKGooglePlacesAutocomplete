5f2447d0c03c997bc04a74778cb92038030e92c2
harlan <hkellaway.tech@gmail.com>
2015-04-26 13:35:22 -0700

    Added test spec for QueryResponsePredictionTerm

---
+++ Example/HNKGooglePlacesAutocomplete-ExampleTests/HNKQueryResponsePreditionTermSpec.m
@@ -0,0 +1,49 @@
+//
+//  HNKQueryResponsePreditionTermSpec.m
+//  HNKGooglePlacesAutocomplete-Example
+//
+//  Created by Harlan Kellaway on 4/26/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+#import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocomplete.h>
+
+SPEC_BEGIN(HNKQueryResponsePreditionTermSpec)
+
+__block HNKQueryResponsePredictionTerm *testInstance;
+__block NSDictionary *json;
+
+beforeAll(^{
+
+    json = @{ @"offset" : @10, @"value" : @"BC" };
+
+    testInstance = [HNKQueryResponsePredictionTerm modelFromJSONDictionary:json];
+
+});
+
+describe(@"HNKQueryResponsePredictionTerm", ^{
+
+    specify(^{
+
+        [[testInstance should] beNonNil];
+
+    });
+
+    describe(@"Deserialization",
+             ^{
+
+                 it(@"Should assign properties correctly",
+                    ^{
+
+                        [[theValue(testInstance.offset) should] equal:theValue(10)];
+                        [[testInstance.value should] equal:@"BC"];
+
+                    });
+
+             });
+
+});
+
+SPEC_END
