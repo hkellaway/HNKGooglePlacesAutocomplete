50c7457111e463a4eddf5750c043e8c0e9de90a4
harlan <hkellaway.tech@gmail.com>
2015-04-26 14:57:48 -0700

    Added QueryResponsePrediction to Pod models

---
+++ Example/Pods/HNKGooglePlacesAutocomplete/Pod/Classes/HNKQueryResponsePrediction.m
@@ -0,0 +1,13 @@
+//
+//  HNKQueryResponsePrediction.m
+//  Pods
+//
+//  Created by Harlan Kellaway on 4/26/15.
+//
+//
+
+#import "HNKQueryResponsePrediction.h"
+
+@implementation HNKQueryResponsePrediction
+
+@end
