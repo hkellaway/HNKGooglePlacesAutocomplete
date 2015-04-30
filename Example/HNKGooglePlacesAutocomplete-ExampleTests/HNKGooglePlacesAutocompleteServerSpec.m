758888a2cc57ca2f6d44c3b2053532dd5f1dc210
harlan <hkellaway.tech@gmail.com>
2015-04-29 19:31:23 -0700

    Started on Server tests

---
+++ Example/HNKGooglePlacesAutocomplete-ExampleTests/HNKGooglePlacesAutocompleteServerSpec.m
@@ -0,0 +1,49 @@
+//
+//  HNKGooglePlacesAutocompleteServerSpec.m
+//  HNKGooglePlacesAutocomplete-Example
+//
+//  Created by Harlan Kellaway on 4/28/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+#import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocompleteServer.h>
+#import <HNKServerFacade/HNKServer.h>
+
+SPEC_BEGIN(HNKGooglePlacesAutocompleteServerSpec)
+
+describe(@"HNKGooglePlacesAutocompleteServer", ^{
+
+    describe(@"Method: initialize",
+             ^{
+
+                 it(@"Should setup HNKServer",
+                    ^{
+                        [[HNKServer should] receive:@selector(setupWithBaseUrl:)
+                                      withArguments:@"https://maps.googleapis.com/maps/api/"];
+
+                        [HNKGooglePlacesAutocompleteServer initialize];
+                    });
+             });
+
+    describe(@"Method: GET:parameters:completion:",
+             ^{
+                 it(@"Should call HNKServer GET",
+                    ^{
+                        [[HNKServer should] receive:@selector(GET:parameters:completion:)
+                                      withArguments:@"place/autocomplete/json",
+                                                    @{ @"input" : @"some town" },
+                                                    any()];
+                        [HNKGooglePlacesAutocompleteServer GET:@"place/autocomplete/json"
+                                                    parameters:@{
+                                                        @"input" : @"some town"
+                                                    }
+                                                    completion:nil];
+
+                    });
+             });
+
+});
+
+SPEC_END
