c9d728340098b0aa7ee36b93779cf29df13042e6
harlan <hkellaway.tech@gmail.com>
2015-04-29 20:58:03 -0700

    Created Query model with setup method

---
+++ Example/Pods/HNKGooglePlacesAutocomplete/Pod/Classes/HNKGooglePlacesAutocompleteQuery.m
@@ -0,0 +1,13 @@
+//
+//  HNKGooglePlacesAutocompleteQuery.m
+//  Pods
+//
+//  Created by Harlan Kellaway on 4/29/15.
+//
+//
+
+#import "HNKGooglePlacesAutocompleteQuery.h"
+
+@implementation HNKGooglePlacesAutocompleteQuery
+
+@end
