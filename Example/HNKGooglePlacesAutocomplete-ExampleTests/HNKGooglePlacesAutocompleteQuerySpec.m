76a6922ba0b4d4a1b8004cca45f62c9c19e88989
harlan <hkellaway.tech@gmail.com>
2015-04-30 22:10:19 -0700

    Added more tests for Query

--- Example/HNKGooglePlacesAutocomplete-ExampleTests/HNKGooglePlacesAutocompleteQuerySpec.m
+++ Example/HNKGooglePlacesAutocomplete-ExampleTests/HNKGooglePlacesAutocompleteQuerySpec.m
@@ -8,7 +8,7 @@
 
 #import "Kiwi.h"
 
-#import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocompleteQuery.h>
+#import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocomplete.h>
 #import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocompleteServer.h>
 
 @interface HNKGooglePlacesAutocompleteQuery (KiwiExposedMethods)
@@ -38,6 +38,7 @@ describe(@"HNKGooglePlacesAutocompleteQuery", ^{
     describe(
         @"Method: fetchPlacesForSearchQuery:completion:",
         ^{
+            typedef void (^HNKGooglePlacesAutocompleteServerCallback)(id JSON, NSError *error);
 
             it(@"Should make a GET request to the Server",
                ^{
@@ -54,6 +55,120 @@ describe(@"HNKGooglePlacesAutocompleteQuery", ^{
                    [[HNKGooglePlacesAutocompleteQuery sharedQuery] fetchPlacesForSearchQuery:@"Vict" completion:nil];
 
                });
+
+            context(
+                @"JSON fetched successfully",
+                ^{
+
+                    beforeEach(^{
+
+                        NSDictionary *testJSON = @{
+                            @"predictions" : @[
+                                @{
+                                   @"description" : @"Victoria, BC, Canadá",
+                                   @"id" : @"d5892cffd777f0252b94ab2651fea7123d2aa34a",
+                                   @"matched_substrings" : @[ @{@"length" : @4, @"offset" : @0} ],
+                                   @"place_id" : @"ChIJcWGw3Ytzj1QR7Ui7HnTz6Dg",
+                                   @"reference" : @"CjQtAAAA903zyJZAu2FLA6KkdC7UAddRHAfHQDpArCk61FI_"
+                                   @"u1Ig7WaJqBiXYsQvORYMcgILEhAFvGtwa5VQpswubIIzwI5wGhTt8vgj6CSQp8QWYb4U1rXmlkg9bg",
+                                   @"terms" : @[
+                                       @{@"offset" : @0, @"value" : @"Victoria"},
+                                       @{@"offset" : @10, @"value" : @"BC"},
+                                       @{@"offset" : @14, @"value" : @"Canadá"}
+                                   ],
+                                   @"types" : @[ @"locality", @"political", @"geocode" ]
+                                }
+                            ],
+                            @"status" : @"OK"
+                        };
+
+                        [HNKGooglePlacesAutocompleteServer stub:@selector(GET:parameters:completion:)
+                                                      withBlock:^id(NSArray *params) {
+
+                                                          HNKGooglePlacesAutocompleteServerCallback completion =
+                                                              params[2];
+                                                          completion(testJSON, nil);
+
+                                                          return nil;
+
+                                                      }];
+
+                    });
+
+                    it(@"Should return Places",
+                       ^{
+
+                           __block HNKQueryResponsePrediction *testPlace;
+
+                           [testInstance fetchPlacesForSearchQuery:@"Vict"
+                                                        completion:^(NSArray *places, NSError *error) {
+                                                            testPlace = places[0];
+                                                        }];
+
+                           [[testPlace.predictionDescription should] equal:@"Victoria, BC, Canadá"];
+
+                           HNKQueryResponsePredictionMatchedSubstring *matchedSubstring =
+                               testPlace.matchedSubstrings[0];
+                           [[theValue(matchedSubstring.length) should] equal:theValue(4)];
+                           [[theValue(matchedSubstring.offset) should] equal:theValue(0)];
+
+                           [[testPlace.placeId should] equal:@"ChIJcWGw3Ytzj1QR7Ui7HnTz6Dg"];
+
+                           HNKQueryResponsePredictionTerm *term1 = testPlace.terms[0];
+                           HNKQueryResponsePredictionTerm *term2 = testPlace.terms[1];
+                           HNKQueryResponsePredictionTerm *term3 = testPlace.terms[2];
+                           [[theValue(term1.offset) should] equal:theValue(0)];
+                           [[term1.value should] equal:@"Victoria"];
+                           [[theValue(term2.offset) should] equal:theValue(10)];
+                           [[term2.value should] equal:@"BC"];
+                           [[theValue(term3.offset) should] equal:theValue(14)];
+                           [[term3.value should] equal:@"Canadá"];
+
+                           [[testPlace.types[0] should] equal:theValue(HNKGooglePlacesAutocompletePlaceTypeLocality)];
+                           [[testPlace.types[1] should] equal:theValue(HNKGooglePlacesAutocompletePlaceTypePolitical)];
+                           [[testPlace.types[2] should] equal:theValue(HNKGooglePlacesAutocompletePlaceTypeGeocode)];
+
+                       });
+
+                });
+
+            context(
+                @"Error during fetch",
+                ^{
+
+                    __block NSError *testError;
+
+                    beforeEach(^{
+
+                        testError = [NSError errorWithDomain:@"Test Domain" code:100 userInfo:@{ @"user" : @"info" }];
+
+                        [HNKGooglePlacesAutocompleteServer stub:@selector(GET:parameters:completion:)
+                                                      withBlock:^id(NSArray *params) {
+
+                                                          HNKGooglePlacesAutocompleteServerCallback completion =
+                                                              params[2];
+                                                          completion(nil, testError);
+
+                                                          return nil;
+
+                                                      }];
+
+                    });
+
+                    it(@"Should return error",
+                       ^{
+                           __block NSError *errorToRecieve;
+
+                           [testInstance fetchPlacesForSearchQuery:@"Vict"
+                                                        completion:^(NSArray *places, NSError *error) {
+                                                            errorToRecieve = error;
+                                                        }];
+
+                           [[errorToRecieve should] equal:testError];
+
+                       });
+
+                });
 
         });
 
