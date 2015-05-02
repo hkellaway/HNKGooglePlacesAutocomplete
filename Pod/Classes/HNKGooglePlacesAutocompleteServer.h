50437bf0c2f013016a9df94a73ff436d7e0a56b0
harlan <hkellaway.tech@gmail.com>
2015-05-02 16:50:58 -0700

    Added documentation for Server

--- Pod/Classes/HNKGooglePlacesAutocompleteServer.h
+++ Pod/Classes/HNKGooglePlacesAutocompleteServer.h
@@ -29,6 +29,13 @@
 
 #pragma mark - Requests
 
+/**
+ *  Performs a GET request to the Server
+ *
+ *  @param path       Path to GET from
+ *  @param parameters Request parameters
+ *  @param completion Block to be executed when the request finishes
+ */
 + (void)GET:(NSString *)path
     parameters:(NSDictionary *)parameters
     completion:(void (^)(id JSON, NSError *error))completion;
