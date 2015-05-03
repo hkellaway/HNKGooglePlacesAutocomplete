0e181fd066e26a36d4e6a7bba95fcbcfbcd4be5b
harlan <hkellaway.tech@gmail.com>
2015-05-02 17:23:39 -0700

    Updated Example project

--- Example/Pods/HNKGooglePlacesAutocomplete/Pod/Classes/HNKGooglePlacesAutocompleteServer.h
+++ Example/Pods/HNKGooglePlacesAutocomplete/Pod/Classes/HNKGooglePlacesAutocompleteServer.h
@@ -25,10 +25,20 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Server setup with base URL for Google Places Autocomplete API
+ */
 @interface HNKGooglePlacesAutocompleteServer : NSObject
 
 #pragma mark - Requests
 
+/**
+ *  Performs a GET request to the Server
+ *
+ *  @param path       Path to GET from
+ *  @param parameters Request parameters
+ *  @param completion Block to be executed when the request finishes
+ */
 + (void)GET:(NSString *)path
     parameters:(NSDictionary *)parameters
     completion:(void (^)(id JSON, NSError *error))completion;
