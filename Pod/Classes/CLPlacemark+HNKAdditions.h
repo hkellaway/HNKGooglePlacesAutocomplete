4e7c8bc1f49c64b09d44638a64a734c7afd50fba
harlan <hkellaway.tech@gmail.com>
2015-05-02 19:14:42 -0700

    Added CLPlacemark category to pod

---
+++ Pod/Classes/CLPlacemark+HNKAdditions.h
@@ -0,0 +1,43 @@
+//
+//  CLPlacemark+HNKAdditions.h
+//  HNKGooglePlacesAutocomplete
+//
+// Copyright (c) 2015 Harlan Kellaway
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+@class HNKQueryResponsePrediction;
+
+@interface CLPlacemark (HNKAdditions)
+
+/**
+ *  Creates a CLPlacemark from an object representing a Google Place
+ *
+ *  @param place Google Place to resolve
+ *
+ *  @return CLPlacemark resolved from Google Place
+ */
++ (instancetype)hnk_placemarkFromGooglePlace:(HNKQueryResponsePrediction *)place
+                                  completion:(void (^)(CLPlacemark *placemark, NSString *addressString,
+                                                       NSError *error))completion;
+
+@end
