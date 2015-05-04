dd9842af8283795fad24971344632900f6a0ac99
harlan <hkellaway.tech@gmail.com>
2015-05-04 07:57:22 -0700

    Added remaining missing place types

--- Pod/Classes/HNKQueryResponsePrediction.h
+++ Pod/Classes/HNKQueryResponsePrediction.h
@@ -50,9 +50,21 @@ typedef NS_ENUM(NSInteger, HNKGooglePlacesAutocompletePlaceType) {
    */
   HNKGooglePlacesAutocompletePlaceTypeAdministrativeAreaLevel3,
   /**
+   *  Places of type Administrative Area Level 4
+   */
+  HNKGooglePlacesAutocompletePlaceTypeAdministrativeAreaLevel4,
+  /**
+   *  Places of type Administrative Area Level 5
+   */
+  HNKGooglePlacesAutocompletePlaceTypeAdministrativeAreaLevel5,
+  /**
    *  Places of type locality or Administrative Area 3
    */
   HNKGooglePlacesAutocompletePlaceTypeCities,
+  /**
+   *  Places of type colloquial area
+   */
+  HNKGooglePlacesAutocompletePlaceTypeColloquialArea,
   /**
    *  Places of type country
    */
@@ -62,21 +74,65 @@ typedef NS_ENUM(NSInteger, HNKGooglePlacesAutocompletePlaceType) {
    */
   HNKGooglePlacesAutocompletePlaceTypeEstablishment,
   /**
+   *  Places of type floor
+   */
+  HNKGooglePlacesAutocompletePlaceTypeFloor,
+  /**
    *  Places from only geocoding results, not business results
    */
   HNKGooglePlacesAutocompletePlaceTypeGeocode,
+  /**
+   *  Places of type intersection
+   */
+  HNKGooglePlacesAutocompletePlaceTypeIntersection,
   /**
    *  Places of type locality
    */
   HNKGooglePlacesAutocompletePlaceTypeLocality,
   /**
+   *  Places of type natural feature
+   */
+  HNKGooglePlacesAutocompletePlaceTypeNaturalFeature,
+  /**
+   *  Places of type neighborhood
+   */
+  HNKGooglePlacesAutocompletePlaceTypeNeighborhood,
+  /**
    *  Places of type political
    */
   HNKGooglePlacesAutocompletePlaceTypePolitical,
   /**
+   *  Places of type point of interest
+   */
+  HNKGooglePlacesAutocompletePlaceTypePointOfInterest,
+  /**
+   *  Places of type post box
+   */
+  HNKGooglePlacesAutocompletePlaceTypePostBox,
+  /**
    *  Places of type postal code
    */
   HNKGooglePlacesAutocompletePlaceTypePostalCode,
+  /**
+   *  Places of type postal code prefix
+   */
+  HNKGooglePlacesAutocompletePlaceTypePostalCodePrefix,
+  /**
+   *  Places of type postal code suffix
+   */
+  HNKGooglePlacesAutocompletePlaceTypePostalCodeSuffix,
+  /**
+   *  Places of type postal town
+   */
+  HNKGooglePlacesAutocompletePlaceTypePostalTown,
+  /**
+   *  Places of type premise
+   */
+  HNKGooglePlacesAutocompletePlaceTypePremise,
+  /**
+   *  Places of type room
+   */
+  HNKGooglePlacesAutocompletePlaceTypeRoom,
   /**
    *  Places of type locality, sub-locality, postal code, country,
    *  Administrative Area Level 1 or Administrative Area Level 2
@@ -89,7 +145,35 @@ typedef NS_ENUM(NSInteger, HNKGooglePlacesAutocompletePlaceType) {
   /**
    *  Places of type sub-locality
    */
-  HNKGooglePlacesAutocompletePlaceTypeSublocality
+  HNKGooglePlacesAutocompletePlaceTypeSublocality,
+  /**
+   *  Places of type sub-locality level 1
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSublocalityLevel1,
+  /**
+   *  Places of type sub-locality level 2
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSublocalityLevel2,
+  /**
+   *  Places of type sub-locality level 3
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSublocalityLevel3,
+  /**
+   *  Places of type sub-locality level 4
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSublocalityLevel4,
+  /**
+   *  Places of type sub-locality level 5
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSublocalityLevel5,
+  /**
+   *  Places of type subpremise
+   */
+  HNKGooglePlacesAutocompletePlaceTypeSubpremise,
+  /**
+   *  Places of type transit station
+   */
+  HNKGooglePlacesAutocompletePlaceTypeTransitStation
 };
 
 /**
