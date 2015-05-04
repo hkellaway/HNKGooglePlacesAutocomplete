4601e858e43ac8935a0a54a01e4793e26063c308
harlan <hkellaway.tech@gmail.com>
2015-05-03 17:17:11 -0700

    Changed prefix of demo classes to HNKDemo

--- Example/HNKGooglePlacesAutocomplete-Example/HNKDemoViewController.m
+++ Example/HNKGooglePlacesAutocomplete-Example/HNKDemoViewController.m
@@ -1,23 +1,23 @@
 //
-//  GPAViewController.m
+//  HNKDemoViewController.m
 //  HNKGooglePlacesAutocomplete-Example
 //
 //  Created by Harlan Kellaway on 4/26/15.
 //  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
 //
 
-#import "GPAViewController.h"
+#import "HNKDemoViewController.h"
 
 #import <CoreLocation/CLPlacemark.h>
 #import <HNKGooglePlacesAutocomplete/HNKGooglePlacesAutocomplete.h>
 
 #import "CLPlacemark+HNKAdditions.h"
 
-@interface GPAViewController ()
+@interface HNKDemoViewController ()
 
 @end
 
-@implementation GPAViewController
+@implementation HNKDemoViewController
 
 - (void)viewDidLoad
 {
