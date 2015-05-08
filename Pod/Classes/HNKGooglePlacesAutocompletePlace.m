cc504b59470a28554b08811e19c3330abae386be
harlan <hkellaway.tech@gmail.com>
2015-05-07 21:59:11 -0700

    Updated podspec

--- Pod/Classes/HNKGooglePlacesAutocompletePlace.m
+++ Pod/Classes/HNKGooglePlacesAutocompletePlace.m
@@ -24,7 +24,7 @@
 //
 
 #import "HNKGooglePlacesAutocompletePlace.h"
-#import "HNKQueryResponsePredictionMatchedSubstring.h"
+#import "HNKGooglePlacesAutocompletePlaceSubstring.h"
 #import "HNKQueryResponsePredictionTerm.h"
 
 #import "NSValueTransformer+MTLPredefinedTransformerAdditions.h"
