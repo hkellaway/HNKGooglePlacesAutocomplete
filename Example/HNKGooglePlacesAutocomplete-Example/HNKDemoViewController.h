8b1b7c2b070232ebe6f9e095d4388a690971c6be
Harlan Kellaway <harlan@prolificinteractive.com>
2015-08-11 20:33:41 -0400

    Changed view controller prefix

--- Example/HNKGooglePlacesAutocomplete-Example/HNKDemoViewController.h
+++ Example/HNKGooglePlacesAutocomplete-Example/HNKDemoViewController.h
@@ -2,7 +2,7 @@
 //  HNKDemoViewController.h
 //  HNKGooglePlacesAutocomplete-Example
 //
-//  Created by Harlan Kellaway on 4/26/15.
+//  Created by Tom OMalley on 8/11/15.
 //  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
 //
 
